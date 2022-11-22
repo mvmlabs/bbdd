940871fd276e349a87ac1d16f2f7f6a3c365e7c3
mvmlabs <57446406+mvmlabs@users.noreply.github.com>
2022-11-22 12:57:36 +0100

    Added
    comentaris i sentència de creació correcta per a taula interrelació M:N

--- UF3/Exemples/casacolonies.sql
+++ UF3/Exemples/casacolonies.sql
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS nen(
 	FOREIGN KEY (id_comarca) REFERENCES comarca(id)
 );
 
+-- Aquesta sentència de creació no és correcta (la PK ha d'estar formada per les dues FK)
 CREATE TABLE IF NOT EXISTS oferta(
 	id INT NOT NULL AUTO_INCREMENT,
 	id_casa INT NOT NULL,
@@ -43,12 +44,24 @@ CREATE TABLE IF NOT EXISTS oferta(
 	FOREIGN KEY (id_casa) REFERENCES casa_colonies(id),
 	FOREIGN KEY (id_activitat) REFERENCES activitat(id)	
 );
+-- Sentència de creació correcta quan 2 FK han de ser la PK d'una taula
+CREATE TABLE IF NOT EXISTS oferta(
+	id_casa INT NOT NULL,
+	id_activitat INT NOT NULL,
+	PRIMARY KEY (id_casa, id_activitat),
+	FOREIGN KEY (id_casa) REFERENCES casa_colonies(id),
+	FOREIGN KEY (id_activitat) REFERENCES activitat(id)	
+);
 
 INSERT INTO activitat VALUES (NULL, 'kayac'), (NULL, 'hípica'), (NULL, 'snorkel'), (NULL, 'vela');
 INSERT INTO comarca VALUES (NULL, 'Bages', 1092, 178885), (NULL, 'Baix Camp', 697, 190973), (NULL, 'Cerdanya', 546, 18192), (NULL, 'Garraf', 185, 150887);
 INSERT INTO casa_colonies VALUES (NULL, 'Albatros', 2, 50), (NULL, 'Castell de Fluvià', 3, 80), (NULL, 'Mas Pinsach', 1, 120), (NULL, 'Torreferrana', 2, 35), (NULL, 'La Garbinada', 1, 55);
 INSERT INTO nen VALUES (NULL, 1, 1, 'Marta', 'Garcia', 651223241), (NULL, 2, 4, 'Joan', 'Puig', 623541122), (NULL, 3, 2, 'Cristina', 'Fernandez', 678554980);
+
+--Aquesta inserció no és correcta (explicat al comentari línia 37)
 INSERT INTO oferta VALUES (NULL, 1, 4), (NULL, 2, 1), (NULL, 3, 2), (NULL, 4, 3);
+
+INSERT INTO oferta VALUES (1, 4), ( 4, 3);
 
 SELECT c.nom_casa, a.nom_activitat FROM oferta o
 INNER JOIN casa_colonies c ON o.id_casa = c.id
@@ -57,4 +70,3 @@ INNER JOIN activitat a ON o.id_activitat = a.id;
 SELECT c.nom_casa as casa, a.nom_activitat as activitat 
 FROM oferta o INNER JOIN casa_colonies c ON o.id_casa = c.id 
 INNER JOIN activitat a ON o.id_activitat = a.id;
-
