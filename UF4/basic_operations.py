9ab9c54d1931f752f5910a3b8eee65d074aec337
mvmlabs <ralaman@institutmvm.cat>
2023-05-14 18:17:28 +0200

    MongoDB first steps

---
+++ UF4/basic_operations.py
@@ -0,0 +1,60 @@
+import pymongo
+from pymongo import MongoClient
+
+MONGODB_URI = 'mongodb+srv://ralaman:xRcHfqkVI2PPOg8g@cluster0.dwgoiwc.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(MONGODB_URI)
+
+
+for db_list in client.list_database_names():
+   print(db_list)
+
+print('*******************************************')
+
+db = client['gestio_articles']
+col = db['articles']
+'''
+col.insert_one({
+    'codi': 'ART001',
+    'nom': 'vestit',
+    'tipus': 'roba',
+    'quantitat': 50,
+    'preu': 15.95
+})
+
+col.insert_many([
+   {
+    'codi': 'ART002',
+    'nom': 'Ulleres',
+    'tipus': 'Complements',
+    'quantitat': 50,
+    'preu': 75.99
+   },
+   {
+    'codi': 'ART003',
+    'nom': 'Sabatilles',
+    'tipus': 'sabates',
+    'quantitat': 200,
+    'preu': 9.95
+   },
+   {
+      'codi': 'ART004',
+    'nom': 'Bikini',
+    'tipus': 'Bany',
+    'quantitat': 75,
+    'preu': 34.95
+   },
+   {
+      'codi': 'ART005',
+    'nom': 'Jaqueta',
+    'tipus': 'Abric',
+    'quantitat': 6,
+    'preu': 29.99
+   },
+])
+'''
+print(db.name)
+
+for x in col.find({"quantitat":{"$gte":10}}):
+    print(x)
+
